376f95a8bdf2168c4895bfb1b5ec0cf9e80a38f5
mguibtech <mguibtech@gmail.com>
2023-10-04 01:03:37 -0400

    Custom fonts Android

--- App.tsx
+++ App.tsx
@@ -5,7 +5,20 @@ import {Text} from './src/components/Text/Text';
 function App(): JSX.Element {
   return (
     <SafeAreaView>
-      <Text preset="headingLarge" style={{color: 'red'}}>Testando</Text>
+      <Text preset="headingLarge" style={{color: 'red'}}>
+        Testando
+      </Text>
+      <Text
+        preset="headingLarge"
+        style={{color: 'red', fontFamily: 'Stardom-Regular'}}>
+        Testando
+      </Text>
+
+      <Text
+        preset="headingLarge"
+        style={{color: 'red', fontFamily: 'Satoshi-Italic'}}>
+        Testando
+      </Text>
     </SafeAreaView>
   );
 }
