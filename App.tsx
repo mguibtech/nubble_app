6e88e9a59fb1af8b5cc999a31ffa6003224a23e4
mguibtech <mguibtech@gmail.com>
2023-10-04 02:15:32 -0400

    Create component of Button and adding theme

--- App.tsx
+++ App.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-import {Text} from './src/components/Text/Text';
+import {SafeAreaView, View} from 'react-native';
+import {Button} from './src/components/Button/Button';
+import {ThemeProvider} from '@shopify/restyle';
+import {theme} from './src/theme/theme';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <Text preset="headingLarge" italic>
-        Testando
-      </Text>
-      <Text preset="headingLarge" semiBold>
-        Testando
-      </Text>
-
-      <Text preset="paragraphCaption">Testando</Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <View style={{paddingHorizontal: 20}}>
+          <Button title="Login" />
+        </View>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
